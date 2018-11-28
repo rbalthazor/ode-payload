678a5407fc6f6262c238c56a13f058014a98c317
gabrielrwilson <44214158+gabrielrwilson@users.noreply.github.com>
2018-11-28 16:49:02 -0700

    Update ode-cmds.h

--- ode-cmds.h
+++ ode-cmds.h
@@ -1,38 +1,38 @@
 #ifndef ODE_CMDS_H
 #define ODE_CMDS_H
 
-define ODE_BLINK_LED_505L_CMD 4
-define ODE_BLINK_LED_505L_RESP (ODE_BLINK_LED_505L_CMD | 0x80)
+#define ODE_BLINK_LED_505L_CMD 4
+#define ODE_BLINK_LED_505L_RESP (ODE_BLINK_LED_505L_CMD | 0x80)
 
-define ODE_BLINK_LED_645L_CMD 5
-define ODE_BLINK_LED_645L_RESP (ODE_BLINK_LED_645L_CMD | 0x80)
+#define ODE_BLINK_LED_645L_CMD 5
+#define ODE_BLINK_LED_645L_RESP (ODE_BLINK_LED_645L_CMD | 0x80)
 
-define ODE_BLINK_LED_851L_CMD 6
-define ODE_BLINK_LED_851L_RESP (ODE_BLINK_LED_851L_CMD | 0x80)
+#define ODE_BLINK_LED_851L_CMD 6
+#define ODE_BLINK_LED_851L_RESP (ODE_BLINK_LED_851L_CMD | 0x80)
 
-define ODE_BLINK_LED_CREE_CMD 7
-define ODE_BLINK_LED_CREE_RESP (ODE_BLINK_LED_CREE_CMD | 0x80)
+#define ODE_BLINK_LED_CREE_CMD 7
+#define ODE_BLINK_LED_CREE_RESP (ODE_BLINK_LED_CREE_CMD | 0x80)
 
-define ODE_BURN_SM_BALL_CMD 8
-define ODE_BURN_SM_BALL_RESP (ODE_BURN_SM_BALL_CMD | 0x80)
+#define ODE_BURN_SM_BALL_CMD 8
+#define ODE_BURN_SM_BALL_RESP (ODE_BURN_SM_BALL_CMD | 0x80)
 
-define ODE_BURN_LG_BALL_CMD 12
-define ODE_BURN_LG_BALL_RESP (ODE_BURN_LG_BALL_CMD | 0x80)
+#define ODE_BURN_LG_BALL_CMD 12
+#define ODE_BURN_LG_BALL_RESP (ODE_BURN_LG_BALL_CMD | 0x80)
 
-define ODE_BURN_MELTWIRE_CMD 13
-define ODE_BURN_MELTWIRE_RESP (ODE_BURN_MELTWIRE_CMD | 0x80)
+#define ODE_BURN_MELTWIRE_CMD 13
+#define ODE_BURN_MELTWIRE_RESP (ODE_BURN_MELTWIRE_CMD | 0x80)
 
-define ODE_READ_MW_FB_CMD 1
-define ODE_READ_MW_FB_LOW_RESP (ODE_READ_MW_FB_CMD | 0x80)
-define ODE_READ_MW_FB_HIGH_RESP (ODE_READ_MW_FB_CMD+0x08 | 0x80) //Can be 1 or 9
+#define ODE_READ_MW_FB_CMD 1
+#define ODE_READ_MW_FB_LOW_RESP (ODE_READ_MW_FB_CMD | 0x80)
+#define ODE_READ_MW_FB_HIGH_RESP (ODE_READ_MW_FB_CMD+0x08 | 0x80) //Can be 1 or 9
 
-define ODE_READ_SM_BALL_FB_CMD 2
-define ODE_READ_SM_BALL_FB_LOW_RESP (ODE_READ_SM_BALL_FB_CMD | 0x80)
-define ODE_READ_SM_BALL_FB_HGH_RESP (ODE_READ_SM_BALL_FB_CMD+0x08 | 0x80) //can be 2 or 10
+#define ODE_READ_SM_BALL_FB_CMD 2
+#define ODE_READ_SM_BALL_FB_LOW_RESP (ODE_READ_SM_BALL_FB_CMD | 0x80)
+#define ODE_READ_SM_BALL_FB_HGH_RESP (ODE_READ_SM_BALL_FB_CMD+0x08 | 0x80) //can be 2 or 10
 
-define ODE_READ_LG_BALL_FB_CMD 3
-define ODE_READ_LG_BALL_FB_LOW_RESP (ODE_READ_LG_BALL_FB_CMD | 0x80)
-define ODE_READ_SM_BALL_FB_HIGH_RESP (ODE_READ_SM_BALL_FB_CMD+0x08 | 0x80) //can be 3 or 11
+#define ODE_READ_LG_BALL_FB_CMD 3
+#define ODE_READ_LG_BALL_FB_LOW_RESP (ODE_READ_LG_BALL_FB_CMD | 0x80)
+#define ODE_READ_SM_BALL_FB_HIGH_RESP (ODE_READ_SM_BALL_FB_CMD+0x08 | 0x80) //can be 3 or 11
 
 struct ODEStatus {
 	uint8_t door_sw;   	//0 for closed, 1 for opened after a successful switch read
@@ -45,8 +45,8 @@ struct ODEStatus {
 	uint8_t LED_IRFB;	//0 for on, 1 for off
 	uint8_t Door;		//0 for an unburnt metlwire, 1 for a burnt meltwire
 	uint8_t SM_Ball;	//0 for an unfired nitonal, 1 if fired	
-	uint8_t LG_Ball		//0 for an unfired nitonal, 1 if fired
-} __attribute__((packed))
+	uint8_t LG_Ball;	//0 for an unfired nitonal, 1 if fired
+} __attribute__((packed));
 
 struct ODEBlinkData {
 	uint32_t period_505L;	//half period of a 50% duty cycle flash, set to 0 for continuous operation
@@ -58,13 +58,13 @@ struct ODEBlinkData {
 	uint32_t period_CREE;
 	int32_t duration_CREE;
 	uint32_t period_IRFB;
-	int32_t duration_IRFB
-} __attribute__((packed))
+	int32_t duration_IRFB;
+} __attribute__((packed));
 
 struct ODEDeployData {
 	uint32_t SM_ball_duration; //Duration of operation for deployments
 	uint32_t LG_ball_duration;
-	uint32_t Door_duration		
-} __attribute__((packed))
+	uint32_t Door_duration;		
+} __attribute__((packed));
 
 #endif
