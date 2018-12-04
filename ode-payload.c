a58e4f11620d1c2c42ea240f206b0af48d99064e
gabrielrwilson <44214158+gabrielrwilson@users.noreply.github.com>
2018-12-04 12:20:51 -0700

    Update ode-payload.c

--- ode-payload.c
+++ ode-payload.c
@@ -26,7 +26,7 @@ struct ODEPayloadState {
 };
 
 static struct ODEPayloadState *state = NULL;
-ODEStatus sc_status = {0,0,0,0,0,0,0,0,0,0,0};
+static struct ODEStatus sc_status = {0,0,0,0,0,0,0,0,0,0,0};
 
 // Function called when a status command is sent
 void payload_status(int socket, unsigned char cmd, void * data, size_t dataLen,
