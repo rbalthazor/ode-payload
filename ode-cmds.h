f954e717898906795efb520300e2fe8758d53a22
gabrielrwilson <44214158+gabrielrwilson@users.noreply.github.com>
2018-12-04 12:59:36 -0700

    Update ode-cmds.h

--- ode-cmds.h
+++ ode-cmds.h
@@ -11,16 +11,16 @@
 #define ODE_BURN_BALL1_RESP (ODE_BURN_BALL1_CMD | 0x80)
 
 struct ODEStatus {
-   uint8_t ball1_sw;
-   uint8_t ball2_sw;
-   uint8_t MW_sw;
+   uint8_t ball1_sw; //0
+   uint8_t ball2_sw; 
+   uint8_t MW_sw;    //2
    uint8_t ball1_fb;
-   uint8_t ball2_fb;
+   uint8_t ball2_fb; //4
    uint8_t MW_fb;
-   uint8_t cree_led;
+   uint8_t cree_led; //6
    uint8_t led_505L;
-   uint8_t led_645L;
-   uint8_t led_851L;
+   uint8_t led_645L; //8
+   uint8_t led_851L; //9
 } __attribute__((packed));
 
 struct ODEBlinkData {
