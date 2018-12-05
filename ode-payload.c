8c3165dc092d2245f71a3b5733638c09973ca2c5
gabrielrwilson <44214158+gabrielrwilson@users.noreply.github.com>
2018-12-05 15:13:57 -0700

    Update ode-payload.c

--- ode-payload.c
+++ ode-payload.c
@@ -80,7 +80,7 @@ static int start_mw_fb(void *arg)
    codes_for_status[5] = -1;
 	
    // Read the GPIO
-  codes_for_status[5]->read(state->Door_Feedback);
+  codes_for_status[5]=read(state->Door_Feedback);
 
    // Reschedule the event
    return EVENT_KEEP;
