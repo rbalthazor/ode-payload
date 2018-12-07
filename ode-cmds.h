1b10f5e56a3687215c72583610cecf4aa8536f7d
gabrielrwilson <44214158+gabrielrwilson@users.noreply.github.com>
2018-12-06 17:10:46 -0700

    Update ode-cmds.h

--- ode-cmds.h
+++ ode-cmds.h
@@ -1,17 +1,17 @@
 #ifndef ODE_CMDS_H
 #define ODE_CMDS_H
 
-#define ODE_BLINK_CREE_CMD 2
-#define ODE_BLINK_CREE_RESP (ODE_BLINK_CREE_CMD | 0x80)
-
-#define ODE_MW_STATUS_CMD 3
-#define ODE_MW_STATUS_RESP (ODE_MW_STATUS_CMD | 0x80)
-
 #define ODE_BLINK_LED_505L_CMD 4
 #define ODE_BLINK_LED_505L_RESP (ODE_BLINK_LED_505L_CMD | 0x80)
 
+#define ODE_BLINK_CREE_CMD 7
+#define ODE_BLINK_CREE_RESP (ODE_BLINK_CREE_CMD | 0x80)
+
 #define ODE_BURN_BALL1_CMD 10
 #define ODE_BURN_BALL1_RESP (ODE_BURN_BALL1_CMD | 0x80)
+
+#define ODE_MW_STATUS_CMD 10
+#define ODE_MW_STATUS_RESP (ODE_MW_STATUS_CMD | 0x80)
 
 struct ODEStatus {
    uint8_t ball1_sw; //0
@@ -23,15 +23,12 @@ struct ODEStatus {
    uint8_t cree_led; //6
    uint8_t led_505L;
    uint8_t led_645L; //8
-   uint8_t led_851L; //9
+   uint8_t led_851L; 
+   uint8_t led_IR;   //10
 } __attribute__((packed));
 
 struct ODEBlinkData {
    uint32_t period;
-   int32_t duration;
-} __attribute__((packed));
-
-struct ODEFeedBackData {
    int32_t duration;
 } __attribute__((packed));
 
